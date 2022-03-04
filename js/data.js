ca67a48e8b5ba3735d48eac7bcc29e1c1dbe9216
Christina Le <christinaah02@gmail.com>
2022-03-03 20:58:41 -0800

    still in progress.

--- js/data.js
+++ js/data.js
@@ -1,1 +1,18 @@
 /* exported data */
+var data = {
+  view: 'product-lists',
+  details: null,
+  select: {},
+  entries: []
+};
+
+var previousEntries = localStorage.getItem('saved-makeup');
+
+if (previousEntries !== null) {
+  data = JSON.parse(previousEntries);
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var makeUpStorage = JSON.stringify(data);
+  localStorage.setItem('saved-makeup', makeUpStorage);
+});
